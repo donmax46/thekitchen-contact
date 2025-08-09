6eefa0e7baf69ee96c51dfb342fd72f6ac64ebef
donmax46 <donmaxim411@gmail.com>
2025-08-09 02:20:09 +0100

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-function toggleDetails(card) {
-    const details = card.querySelector('.product-details');
-    if (details.style.display === "block") {
-        details.style.display = "none";
-    } else {
-        details.style.display = "block";
-    }
-}
