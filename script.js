6865c7ff72912b87affcee338e8cd036cf022760
donmax46 <donmaxim411@gmail.com>
2026-05-20 00:33:11 +0100

    Update script.js

--- script.js
+++ script.js
@@ -3,9 +3,9 @@ window.addEventListener("scroll", () => {
   const navbar = document.querySelector(".navbar");
 
   if(window.scrollY > 50){
-    navbar.style.background = "rgba(0,0,0,0.85)";
+    navbar.style.background = "rgba(0,0,0,0.95)";
   } else {
-    navbar.style.background = "rgba(0,0,0,0.35)";
+    navbar.style.background = "rgba(0,0,0,0.7)";
   }
 
 });
