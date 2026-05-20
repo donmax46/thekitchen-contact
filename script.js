79c44416cbcd051bbafdca3787ceb061c4d9d6b4
donmax46 <donmaxim411@gmail.com>
2026-05-20 17:04:22 +0100

    Update script.js

--- script.js
+++ script.js
@@ -3,9 +3,62 @@ window.addEventListener("scroll", () => {
   const navbar = document.querySelector(".navbar");
 
   if(window.scrollY > 50){
-    navbar.style.background = "rgba(0,0,0,0.95)";
+
+    navbar.style.background =
+    "rgba(0,0,0,0.95)";
+
   } else {
-    navbar.style.background = "rgba(0,0,0,0.7)";
+
+    navbar.style.background =
+    "rgba(0,0,0,0.7)";
+
   }
+
+});
+
+/* LOADER */
+
+window.addEventListener("load", () => {
+
+  const loader =
+  document.getElementById("loader");
+
+  setTimeout(() => {
+
+    loader.style.opacity = "0";
+
+    setTimeout(() => {
+
+      loader.style.display = "none";
+
+    },1000);
+
+  },1800);
+
+});
+
+/* SCROLL REVEAL */
+
+const reveals =
+document.querySelectorAll("section");
+
+window.addEventListener("scroll", () => {
+
+  reveals.forEach((section) => {
+
+    const windowHeight =
+    window.innerHeight;
+
+    const revealTop =
+    section.getBoundingClientRect().top;
+
+    if(revealTop < windowHeight - 100){
+
+      section.classList.add("reveal");
+      section.classList.add("active");
+
+    }
+
+  });
 
 });
